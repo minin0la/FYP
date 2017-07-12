c24daeefbc6054210c444bec2018c5a5fe92503b
Natawat Hebnak <minin0la17@gmail.com>
2017-07-12 23:39:02 +0800

    import Image
    Please type "pip install Image" in your terminal and change the location of the picture before running the script

--- FridgeBud/GUI.py
+++ FridgeBud/GUI.py
@@ -1,16 +1,18 @@
 # This will be use for GUI of the software
 # !/usr/bin/python
 import Tkinter as tk
+from PIL import Image, ImageTk
+
+root = tk.Tk()
 
 
 class FridgeBud_tk(tk.Tk):
     root.title("FridgeBud")
     root.geometry("1920x1080")
-    root = tk.Tk()
-    photo = tk.PhotoImage(file=r"/Users/kennethwong/Desktop/FYP")
 
 
+photo = ImageTk.PhotoImage(Image.open("/Users/minin0la/Documents/GIt/FYP/FridgeBud/Milk.JPG"))
 cv = tk.Canvas()
 cv.pack(side='top', fill='both', expand='yes')
-cv.create_image(10, 10, image='Milk', anchor='nw')
+cv.create_image(10, 10, image=photo, anchor='nw')
 root.mainloop()
