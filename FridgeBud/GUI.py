26d79cd07d39e2ddd12fd1ff0d7819b348939be2
Kenneth Wong <kennethunder706@gmail.com>
2017-07-12 22:35:36 +0800

    Root Error

--- FridgeBud/GUI.py
+++ FridgeBud/GUI.py
@@ -1,11 +1,16 @@
 # This will be use for GUI of the software
 # !/usr/bin/python
+import Tkinter as tk
 
-import Tkinter
-# create window
-root = Tkinter.Tk()
-# modify root window
-root.title("FridgeBud")
-root.geometry("1920x1080")
-# kick off the event loop
+
+class FridgeBud_tk(tk.Tk):
+    root.title("FridgeBud")
+    root.geometry("1920x1080")
+    root = tk.Tk()
+    photo = tk.PhotoImage(file=r"/Users/kennethwong/Desktop/FYP")
+
+
+cv = tk.Canvas()
+cv.pack(side='top', fill='both', expand='yes')
+cv.create_image(10, 10, image='Milk', anchor='nw')
 root.mainloop()
