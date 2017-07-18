117f49f82c02f196b6993a5cb79da037e8c05c41
Kenneth Wong <kennethunder706@gmail.com>
2017-07-18 14:20:24 +0800

    Nothing

--- FridgeBud/GUI.py
+++ FridgeBud/GUI.py
@@ -2,6 +2,7 @@
 # !/usr/bin/python
 import Tkinter as tk
 from PIL import Image, ImageTk
+root = tk.Tk()
 
 root = tk.Tk()
 
@@ -11,8 +12,26 @@ class FridgeBud_tk(tk.Tk):
     root.geometry("1920x1080")
 
 
-photo = ImageTk.PhotoImage(Image.open("/Users/minin0la/Documents/GIt/FYP/FridgeBud/Milk.JPG"))
-cv = tk.Canvas()
-cv.pack(side='top', fill='both', expand='yes')
-cv.create_image(10, 10, image=photo, anchor='nw')
+cv = tk.Canvas(root, width=1920, height=1080)
+cv.pack()
+
+photo1 = ImageTk.PhotoImage(Image.open
+                            ("/Users/kennethwong/Desktop/FYP/Milk.JPG"))
+photo2 = ImageTk.PhotoImage(Image.open
+                            ("/Users/kennethwong/Desktop/FYP/Fruits.JPeG"))
+photo3 = ImageTk.PhotoImage(Image.open
+                            ("/Users/kennethwong/Desktop/FYP/Vegetables.JPG"))
+photo4 = ImageTk.PhotoImage(Image.open
+                            ("/Users/kennethwong/Desktop/FYP/Fish.JPG"))
+photo5 = ImageTk.PhotoImage(Image.open
+                            ("/Users/kennethwong/Desktop/FYP/Meat.JPG"))
+photo6 = ImageTk.PhotoImage(Image.open
+                            ("/Users/kennethwong/Desktop/FYP/Drinks.JPeG"))
+
+cv.create_image(170, 170, image=photo1, anchor='center')
+cv.create_image(170, 500, image=photo2, anchor='center')
+cv.create_image(600, 200, image=photo3, anchor='center')
+cv.create_image(630, 500, image=photo4, anchor='center')
+cv.create_image(1050, 200, image=photo5, anchor='center')
+cv.create_image(1050, 500, image=photo6, anchor='center')
 root.mainloop()
