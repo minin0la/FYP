4734c079392d076bc2cb6fddb1fc5f3c0db0a45d
Natawat Hebnak <minin0la17@gmail.com>
2017-10-02 23:13:08 +0800

    top

--- FridgeBud/V1.0/GUI.py
+++ FridgeBud/V1.0/GUI.py
@@ -67,7 +67,7 @@ class FridgeBud:
         top.geometry("1024x600+139+157")
         # wed, heg = top.winfo_screenwidth(), top.winfo_screenheight()
         # top.geometry("%dx%d+0+0" % (wed, heg))
-        root.attributes('-fullscreen', True)
+        top.attributes('-fullscreen', True)
         top.title("FridgeBud")
         top.configure(background="#d9d9d9")
         top.configure(highlightbackground="#d9d9d9")
